3d1b5e9a931e96f52c15c51ec446666c1207975b
Johan Faerch <johan@faer.ch>
2018-10-25 10:31:25 +0200

    working 02 assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'Angular';
+  userName = '';
+  disabled = true;
+
+  myChange(event) {
+    console.log(event);
+    if (event.length > 0) {
+        this.disabled = false;
+    } else {
+        this.disabled = true;
+    }
+  }
+
+  onResetUserName() {
+      this.userName = '';
+      this.myChange(event);
+  }
 }
