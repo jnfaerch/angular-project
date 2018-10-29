26b8d22e933d3f81dd8d64274bfed49e6cd5d5f6
Johan Faerch <johan@faer.ch>
2018-10-29 15:51:09 +0100

    implementation of components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe(
+            'A test recipe',
+            'Simply the first recipe',
+            'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg'
+        ),
+        new Recipe(
+            'Another test recipe',
+            'Simply the second recipe',
+            'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg'
+        )
+    ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
